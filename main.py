bbf1dac75a47d85213a3f01386c8ec5c7694f4e8
narhz <narhz@pm.me>
2020-01-01 17:50:11 -0600

    assigned response dict values to variables, add agents for assignee IDs

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import json
 auth_token="e0ef9683503f396f145e872c730d79eb"
 org_id="694212024"
 
+AGENTS = {
+    '419932000000128292': 'Seth',
+    '419932000001637008': 'Alex',
+    '419932000000155091': 'Michelle'
+    'Jake': ''
+}
+
 params="sortBy=-createdTime&limit=15"
 
 headers={
@@ -18,14 +25,25 @@ request = requests.get('https://desk.zoho.com/api/v1/tickets?' + params, headers
 if request.status_code == 200:
     tickets =  json.loads(request.content.decode('utf-8'))
     for ticket in tickets.get('data'):
-        print('#' + ticket.get('ticketNumber') + ' - ' + ticket.get('subject'))
-        print(ticket.get('createdTime'))
-        print('Status - ' + ticket.get('status'))
-        if ticket.get('closedTime') == None:
-            pass
+        status = ticket.get('status')
+        number = ticket.get('ticketNumber')
+        create_time = ticket.get('createdTime')
+        close_time = ticket.get('closedTime')
+        subject = ticket.get('subject')
+        agent = ticket.get('assigneeId')
+
+        print('#' + number + ' - ' + subject)
+        print('Created at - ' + create_time)
+        print('Status - ' + status)
+
+        if agent:
+            print('Assigned to - ' + agent)
         else:
-            print('Closed at - ' + ticket.get('closedTime'))
-            
+            print('Unassigned')
+
+        if close_time:
+            print('Closed at - ' + close_time)
+
         print('------------------')
 else:
     pprint("Request not successful. Response code ", request.status_code, " \nResponse : ", request.content)
